2920789fb912266022e76ea703a727b77ce4940d
anurag0511 <anurag051194@gmail.com>
2020-02-20 23:04:17 -0600

    Updated the file to test the functionality of all the C++ methods

--- hardware_emulated/check_ctypes.py
+++ hardware_emulated/check_ctypes.py
@@ -1,30 +1,48 @@
 import sys, platform
 import ctypes, ctypes.util
-#import ConvFC
 from ctypes import cdll
 import numpy as np
 
-
-#path_libc = ctypes.util.find_library("ConvFC")
 Fixedlib = ctypes.CDLL("FixedPoint.so")
-#getattr(cdll.msvcrt)
-#Fixedlib.main()
 
 Fixedlib.Float_to_Fixed.restype = ctypes.c_int32
 Fixedlib.Float_to_Fixed.argtypes = (ctypes.c_float, ctypes.c_int, ctypes.c_int)
+
+Fixedlib.Fixed_to_Float.restype = ctypes.c_float
+Fixedlib.Fixed_to_Float.argtypes = (ctypes.c_float, ctypes.c_int)
+
+Fixedlib.Fixed_Mul.restype = ctypes.c_int32
+Fixedlib.Fixed_Mul.argtypes = (ctypes.c_float, ctypes.c_float, ctypes.c_int, ctypes.c_int)
+
+Fixedlib.Fixed_ACC.restype = ctypes.c_float
+Fixedlib.Fixed_ACC.argtypes = (np.ctypeslib.ndpointer(dtype=np.float32), ctypes.c_int)
+
 def Float_to_Fixed(number, integer, fraction):
     results = Fixedlib.Float_to_Fixed(number, integer, fraction)
     return(results)
 
+def Fixed_to_Float(number, fraction):
+    result = Fixedlib.Fixed_to_Float(number, fraction)
+    return result
 
-# try:
-#     libc = ctypes.CDLL(path_libc)
-# except OSError:
-#     print ('Cannot load the library')
-#     sys.exit()
-#Convlib = cdll.LoadLibrary("ConvFC.so")
-#Convlib.main()
-float_to_fixed = Float_to_Fixed(0.7,1,10)
+def Fixed_Mul(input1, input2, integer, fraction):
+    result = Fixedlib.Fixed_Mul(input1, input2, integer, fraction)
+    return result
+
+def Fixed_ACC(Product, shape):
+    result = Fixedlib.Fixed_ACC(Product, shape)
+    return result
+
+float_to_fixed = Float_to_Fixed(0.7,1,8)
 print (float_to_fixed)
-#print (Fixedlib.Float_to_Fixed(7.2345,3,7))
-#print (f'was able to load the c library from')
+
+fixed_to_float = Fixed_to_Float(float_to_fixed, 8)
+print (fixed_to_float)
+
+mult_result = Fixed_Mul(0.4,0.6,1,10)
+mult_result2 = Fixed_Mul(0.5,0.7,1,10)
+print (mult_result, mult_result2)
+
+a = np.array([mult_result,mult_result2], dtype=np.float32)
+acc_result = Fixed_ACC(a,2)
+print (acc_result)
