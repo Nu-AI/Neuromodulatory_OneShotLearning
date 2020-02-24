f983ac92a9f1a4593279464d4c496b24444dafce
anurag0511 <anurag051194@gmail.com>
2020-02-24 15:40:07 -0600

    Adding the updated and corrected convolutional layer which is functional

--- hardware_emulated/conv.py
+++ hardware_emulated/conv.py
@@ -2,7 +2,6 @@ import numpy as np
 import os, sys
 import decimal
 
-
 import skimage.data
 from skimage.transform import rescale, resize
 
@@ -54,26 +53,36 @@ def Fixed_ACC(Product, shape):
 image = skimage.data.chelsea()
 img  = skimage.color.rgb2gray(image)
 l1_filter = np.zeros((2,3,3))
+l1_filter_new = np.zeros((2,3,3))
 img = skimage.transform.resize(img, (31,31))
 img = img/np.linalg.norm(img)
 norm_image = map(lambda row:row/np.linalg.norm(row), img)
-print (norm_image, img, "This is the norm image")
+#print (norm_image, img, "This is the norm image")
 print(img.shape, " %%%%%%%%%")
 
 l1_filter[0, :, :] = np.array([[[-1, 0, 1],
-                                  [-1, 0, 1],
+                                  [1, 0, 1],
                                   [-1, 0, 1]]])
 
-l1_filter[1, :, :] = np.array([[[1,   1,  1],
+l1_filter[1, :, :] = np.array([[[1,   1,  -1],
                                 [0,   0,  0],
-                               [-1, -1, -1]]])
+                               [-1, 1, 1]]])
+
+l1_filter_new[0, :, :] = np.array([[[1, -1, 1],
+                                  [0, 0, 1],
+                                  [1, 0, -1]]])
+
+l1_filter_new[1, :, :] = np.array([[[1,   0,  -1],
+                                [0,   1,  0],
+                               [-1, 0, 1]]])
+
 l2_filter =  np.zeros((2,2,3,3))
 l2_filter[0,:,:,:] = l1_filter
-l2_filter[1,:,:,:] = l1_filter
+l2_filter[1,:,:,:] = l1_filter_new
 
 ####################################################################
 # This class will take in the following arguments with the input   #
-# image being the input_map, the kernel size,  the stride and  the #
+# image being the input_map, the kernel size, the stride and  the  #
 # number of filters for that particular layer.                     #
 ####################################################################
 
@@ -98,15 +107,18 @@ class conv_3_3:
         temp_feature_map = np.zeros((feature_map_size, feature_map_size, self.nbfilters))
 
         #Starting the convolutions
+        count = 0
         for ftr in range(self.nbfilters):
             print ("Entered thhe looop *******************", self.nbfilters, ftr)
             temp_filter = filter[ftr,:]
             print (temp_filter.shape)
             if (len(self.input_map.shape)>2):
-                print ("Larger map shape", self.input_map.shape)
+                #print ("Larger map shape", self.input_map.shape)
                 #reshaped_filter = np.reshape(filter, (3,3,2))
                 conv_map = self.conv_perform(self.input_map[:,:,0], temp_filter[0,:,:])
                 for channel in range(1, self.input_map.shape[-1]):
+                    count += channel
+                    print ("Entering the inner loop for accumulating results from feature maps", count,self.input_map[:,:,channel].shape)
                     conv_map  = conv_map + self.conv_perform(self.input_map[:,:,channel], temp_filter[channel,:,:])
             else:
                 conv_map = self.conv_perform(self.input_map,temp_filter)
@@ -139,29 +151,33 @@ class conv_3_3:
 
         input_map_size_row = input_map.shape[0]
         input_map_size_col = input_map.shape[1]
-        for t in range(0, input_map_size_col - 1, self.stride):
-            for k in range (0, input_map_size_row-1, self.stride):
+        for t in range(0, input_map_size_col - self.kernel_size+1, self.stride):
+            for k in range (0, input_map_size_row-self.kernel_size+1, self.stride):
                 for i in range (self.kernel_size):
                     for j in range (self.kernel_size):
-                        point_wise_mult += input_map[i][j] * filter[i][j]
+                        point_wise_mult += input_map[k+i][t+j] * filter[i][j]
                         point_wise_mult_fixed += Fixed_Mul(input_map[i][j],filter[i][j],2,10)
                         val_in_float = Fixed_to_Float2(point_wise_mult_fixed, 10)
                 feature_map[temp_counter_1][temp_counter_2] = point_wise_mult
                 feature_map_fixed[temp_counter_1][temp_counter_2] = point_wise_mult_fixed
                 feature_map_float[temp_counter_1][temp_counter_2] = val_in_float
                 temp_counter_1 += 1
+                point_wise_mult = 0
             temp_counter_2 += 1
             temp_counter_1 = 0
         temp_array = feature_map - feature_map_float
-        print (temp_array, "the difference in precision values")
-        #print (feature_map, "this is the feature map in fixed point")
-        #print (feature_map_float, "this is the feature map in float")
+        #print (feature_map, "the difference in precision values")
         return feature_map
 
-
-c = conv_3_3(img, 3,2,2)
-feature_maps = c.forward(l1_filter)
-print (feature_maps.shape)
-d  = conv_3_3(feature_maps, 3,2,2)
-new_feature_maps= d.forward(l2_filter)
-print(new_feature_maps.shape)
+conv1 = conv_3_3(img, 3,2,2)
+feature_maps = conv1.forward(l1_filter)
+print (feature_maps[:,:,0], feature_maps.shape)
+conv2  = conv_3_3(feature_maps, 3,2,2)
+new_feature_maps= conv2.forward(l2_filter)
+print(new_feature_maps,new_feature_maps.shape)
+conv3  = conv_3_3(new_feature_maps, 3,2,2)
+updated_f_maps = conv3.forward(l2_filter)
+print (updated_f_maps,updated_f_maps.shape)
+conv4 = conv_3_3(updated_f_maps,3,2,2)
+updated_final_maps = conv4.forward(l2_filter)
+print(updated_final_maps,updated_final_maps.shape)
