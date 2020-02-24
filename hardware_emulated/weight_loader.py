9bd230ec603ea6fb596c44ab354a643ca2f2d9d0
anurag0511 <anurag051194@gmail.com>
2020-02-23 19:14:27 -0600

    Adding the updated weight loading function

--- hardware_emulated/weight_loader.py
+++ hardware_emulated/weight_loader.py
@@ -6,23 +6,31 @@ import pickle
 import numpy as np
 
 ttype = torch.cuda.FloatTensor;
-suffix="_Wactiv_tanh_alpha_free_flare_0_gamma_0.666_imgsize_31_ipd_0_lr_3e-05_nbclasses_5_nbf_64_nbiter_5000000_nbshots_1_prestime_1_prestimetest_1_rule_oja_steplr_1000000.0_rngseed_"+"1"+"_5000000"
-with open('../results/results'+suffix+'.dat', 'rb') as fo:
-    tmpw = torch.nn.Parameter(torch.from_numpy(pickle.load(fo)).type(ttype))
-    tmpalpha = torch.nn.Parameter(torch.from_numpy(pickle.load(fo)).type(ttype))
-    tmpeta = torch.nn.Parameter(torch.from_numpy(pickle.load(fo)).type(ttype))
 
-print (tmpw.shape, tmpalpha.shape, tmpeta.shape)
-print ("The final layer weighs are ", tmpw)
+#######################################################################################
+# This method reads the final layer weights from a pickle file and converts the torch #
+# tensor to nmupy arrays. Returns th weights, alpha param and learning factor         #
+#######################################################################################
 
-dict = {}
-dict = torch.load('../torchmodels/torchmodel'+suffix + '.txt')
-new_dict={}
-for i in dict:
-    new_dict[i] = dict[i].cpu().numpy()
-    print (i)
-y = tmpw.cpu().detach().numpy()
+def read_fc_params(seed):
 
-print (new_dict['cv1.weight'].shape, new_dict['cv1.bias'].shape)
+    suffix="_Wactiv_tanh_alpha_free_flare_0_gamma_0.666_imgsize_31_ipd_0_lr_3e-05_nbclasses_5_nbf_64_nbiter_5000000_nbshots_1_prestime_1_prestimetest_1_rule_oja_steplr_1000000.0_rngseed_"+str(seed)+"_5000000"
+    with open('../results/results'+suffix+'.dat', 'rb') as fo:
+        tmpw = torch.nn.Parameter(torch.from_numpy(pickle.load(fo)).type(ttype))
+        tmpalpha = torch.nn.Parameter(torch.from_numpy(pickle.load(fo)).type(ttype))
+        tmpeta = torch.nn.Parameter(torch.from_numpy(pickle.load(fo)).type(ttype))
+    dict = torch.load('../torchmodels/torchmodel'+suffix + '.txt')
+    for i in dict:
+        dict[i] = dict[i].cpu().numpy()
+    return dict, tmpw.cpu().detach().numpy(), tmpalpha.cpu().detach().numpy(), tmpeta.cpu().detach().numpy()
+
+#print (tmpw.shape, tmpalpha.shape, tmpeta.shape)
+#print ("The final layer weighs are ", tmpw)
+dict1 = {}
+dict1, tmpw, tmpalpha, tmpeta = read_fc_params(1)
+
+
+print (dict1['cv1.weight'].shape, dict1['cv1.bias'].shape)
+
 #print (dict['w'])
 #print  (dict)
