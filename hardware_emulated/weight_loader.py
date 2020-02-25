eb91f58a1be00c4cfbf5b0d7bf3721fbd6495bfe
anurag0511 <anurag051194@gmail.com>
2020-02-24 22:15:34 -0600

    Updated for minor bug fixes

--- hardware_emulated/weight_loader.py
+++ hardware_emulated/weight_loader.py
@@ -32,4 +32,4 @@ weight_load = weight_loader()
 dict1 = {}
 dict1, tmpw, tmpalpha, tmpeta =weight_load.read_fc_params(1)
 
-print (dict1['cv1.weight'].shape, dict1['cv1.bias'].shape)
+print (dict1['cv2.weight'].shape, dict1['cv2.bias'].shape)
