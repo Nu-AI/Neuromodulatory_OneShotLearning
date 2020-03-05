3f73c8c77781d484af5a76c3228f85a195487bfb
anurag0511 <anurag051194@gmail.com>
2020-03-04 21:52:42 -0600

    Updated the forward pass function with the trace

--- hardware_emulated/fc_layer.py
+++ hardware_emulated/fc_layer.py
@@ -11,16 +11,24 @@ from conv import *
 
 class FC_layer:
 
-    def __init__(self, inputlabel):
-        self.inputs = inputs
+    def __init__(self, input_activs, inputlabel):
+        self.input_activs = input_activs
+        self.inputlabel = inputlabel
 
     def forward(self, weights, alpha, mod):
         mod = np.zeros_like(weights)
         plastic_wt = np.zeros_like(weights)
         plastic_wt_fixed = np.zeros_like(weights)
-        activation = []
+        activation = np.zeros((weights.shape[1]))
         for i in  range(weights.shape[1]):
             for j in range(weights.shape[0]):
                 plastic_wt[j][i] = weights[j][i] + alpha[j][i]*mod[j][i]
                 plastic_wt_fixed[j][i] = Float_to_Fixed(weights[j][i],6,10) + Fixed_Mul(alpha[j][i],mod[j][i],6,10)
-                plastic_wt_fixed[j][i] = Fixed_to_Float2(plastic_wt_fixed[j][i])
+                plastic_wt_fixed[j][i] = Fixed_to_Float2(plastic_wt_fixed[j][i],10)
+
+        for i in range(weights.shape[1]):
+            for j in range(weights.shape[0]):
+                activation[i] += self.input_activs[j]*plastic_wt[j][i]
+                activation_fixed[i] += Fixed_mul(self.input_activs[j],plastic_wt_fixed[j][i],6,10)
+
+            activation_fixed[i] = Fixed_to_Float2(activation_fixed[i],10)
