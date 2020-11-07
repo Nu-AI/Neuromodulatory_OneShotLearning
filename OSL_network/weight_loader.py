a73c0a129c04fb390597b0ff63e57e5140c8122b
anurag0511 <anurag051194@gmail.com>
2020-11-06 21:01:33 -0500

    Adding the new weight loader

---
+++ OSL_network/weight_loader.py
@@ -0,0 +1,23 @@
+import torch
+import pickle
+
+class WeightLoader():
+
+	def __init__(self, params):
+		self.params = params
+
+	def read_fc_params(self, seed):
+		if self.params['cuda']:
+			ttype = torch.cuda.FloatTensor
+		else:
+			ttype = torch.FloatTensor
+		suffix = "_Wactiv_tanh_alpha_free_flare_0_gamma_0.666_imgsize_31_ipd_0_lr_3e-05_nbclasses_5_nbf_64_nbiter_5000000_nbshots_1_prestime_1_prestimetest_1_rule_oja_steplr_1000000.0_rngseed_" + str(
+			seed) + "_5000000"
+		with open('../results/results' + suffix + '.dat', 'rb') as fo:
+			tmpw = torch.nn.Parameter(torch.from_numpy(pickle.load(fo)).type(ttype))
+			tmpalpha = torch.nn.Parameter(torch.from_numpy(pickle.load(fo)).type(ttype))
+			tmpeta = torch.nn.Parameter(torch.from_numpy(pickle.load(fo)).type(ttype))
+		dict = torch.load('../torchmodels/torchmodel' + suffix + '.txt')
+		for i in dict:
+			dict[i] = dict[i].cpu().numpy()
+		return dict, tmpw.cpu().detach().numpy(), tmpalpha.cpu().detach().numpy(), tmpeta.cpu().detach().numpy()
